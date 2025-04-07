83396679afc85ee6317984cd5dff3971c97ac2e8
Ricardo <ricardod7i@hotmail.com>
2025-04-07 14:19:00 -0300

    arreglando ejercicio 8

--- src/tp_2/ejercicio_8/Ejercicio.java
+++ src/tp_2/ejercicio_8/Ejercicio.java
@@ -3,8 +3,8 @@ package tp_2.ejercicio_8;
 import tp_2.ejercicio_2.BinaryTree;
 
 public class Ejercicio {
-	
-	public static void main (String[] args) {
+
+	public static void main(String[] args) {
 		BinaryTree<Integer> arbol1 = getArbol1();
 		BinaryTree<Integer> arbol2 = getArbol2();
 
@@ -36,7 +36,7 @@ public class Ejercicio {
 
 		return raiz;
 	}
-	
+
 	// cambiar este:
 
 	private static BinaryTree<Integer> getArbol2() {
@@ -46,12 +46,24 @@ public class Ejercicio {
 		BinaryTree<Integer> nodo_2 = new BinaryTree<Integer>();
 		BinaryTree<Integer> nodo_3 = new BinaryTree<Integer>();
 		BinaryTree<Integer> nodo_4 = new BinaryTree<Integer>();
+		BinaryTree<Integer> nodo_5 = new BinaryTree<Integer>();
+		BinaryTree<Integer> nodo_6 = new BinaryTree<Integer>();
+		BinaryTree<Integer> nodo_7 = new BinaryTree<Integer>();
+		BinaryTree<Integer> nodo_8 = new BinaryTree<Integer>();
+		BinaryTree<Integer> nodo_9 = new BinaryTree<Integer>();
+		BinaryTree<Integer> nodo_10 = new BinaryTree<Integer>();
 
 		// aristas
 		raiz.addLeftChild(nodo_1);
 		raiz.addRightChild(nodo_2);
 		nodo_1.addRightChild(nodo_3);
+		nodo_1.addLeftChild(nodo_5);
 		nodo_2.addRightChild(nodo_4);
+		nodo_2.addLeftChild(nodo_8);
+		nodo_5.addRightChild(nodo_7);
+		nodo_5.addLeftChild(nodo_6);
+		nodo_4.addLeftChild(nodo_9);
+		nodo_4.addRightChild(nodo_10);
 
 		// datos
 		raiz.setData(65);
@@ -59,7 +71,12 @@ public class Ejercicio {
 		nodo_2.setData(81);
 		nodo_3.setData(47);
 		nodo_4.setData(93);
-
+		nodo_5.setData(22);
+		nodo_8.setData(76);
+		nodo_6.setData(11);
+		nodo_7.setData(29);
+		nodo_9.setData(85);
+		nodo_10.setData(94);
 		return raiz;
 	}
 
--- src/tp_2/ejercicio_8/ParcialArboles.java
+++ src/tp_2/ejercicio_8/ParcialArboles.java
@@ -5,35 +5,15 @@ import tp_2.ejercicio_2.BinaryTree;
 public class ParcialArboles {
 
 	public boolean esPrefijo(BinaryTree<Integer> arbol1, BinaryTree<Integer> arbol2) {
-		boolean result = false;
-		Integer dato_1 = (arbol1 != null) ? arbol1.getData() : null;
-		Integer dato_2 = (arbol2 != null) ? arbol2.getData() : null;
-		
-
-		// compruebo que el dato sea igual
-		result = dato_1 == dato_2;
-
-		// compruebo estructura y datos de hijos
-		if (result) {
-			// verifico arbol izquierdo
-			BinaryTree<Integer> arbol1Izq = (arbol1 != null && arbol1.hasLeftChild()) ? arbol1.getLeftChild() : null;
-			BinaryTree<Integer> arbol2Izq = (arbol2 != null && arbol2.hasLeftChild()) ? arbol2.getLeftChild() : null;
-
-			result = ((arbol1Izq != null) || (arbol1Izq  != null)) ? esPrefijo(arbol1Izq, arbol2Izq) : result;
-			if (result) {
-				// en caso de que arbol izquierdo sea correcto
-				// verifico arbol derecho
-				BinaryTree<Integer> arbol1Der = (arbol1 != null && arbol1.hasRightChild()) ? arbol1.getRightChild()
-						: null;
-				BinaryTree<Integer> arbol2Der = (arbol2 != null && arbol2.hasRightChild()) ? arbol2.getRightChild()
-						: null;
-
-				result = ((arbol1Der != null) || (arbol2Der  != null)) ? esPrefijo(arbol1Der, arbol2Der) : result;
-
-			}
+		if (arbol1 == null) {
+			return true;
+		} else if (arbol2 == null) {
+			return false;
+		} else {
+			return (arbol1.getData() == arbol2.getData()) 
+					&& esPrefijo(arbol1.getLeftChild(), arbol2.getLeftChild())
+					&& esPrefijo(arbol1.getRightChild(), arbol2.getRightChild());
 		}
-
-		return result;
-
 	}
+
 }
